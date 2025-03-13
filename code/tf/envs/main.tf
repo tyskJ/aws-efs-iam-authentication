0b5d366bb35c20bf1d343c466d4845f9c1d9bbd7
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-03-13 22:41:55 +0900

    iam modules

--- code/tf/envs/main.tf
+++ code/tf/envs/main.tf
@@ -2,6 +2,7 @@
 # ║ EFS IAM Authentication Stack - Terraform main.tf module                                                                                          ║
 # ╠═════════════════╤═══════════════════════════════════╤════════════════════════════════════════════════════════════════════════════════════════════╣
 # ║ nw              │ ../modules/vpc_subnet             │ invoke vpc subnet module.                                                                  ║
+# ║ iam             │ ../modules/iam                    │ invoke IAM module.                                                                         ║
 # ╚═════════════════╧═══════════════════════════════════╧════════════════════════════════════════════════════════════════════════════════════════════╝
 
 module "nw" {
@@ -10,4 +11,10 @@ module "nw" {
   vpc_map         = { "name" = "vpc", "cidr_block" = "10.0.0.0/16", "enable_dns_support" = true, "enable_dns_hostnames" = true }
   subnet_map_list = [{ "name" = "public-subnet-a", "cidr_block" = "10.0.1.0/24", "availability_zone" = "${local.region_name}a", "map_public_ip_on_launch" = true }, { "name" = "private-subnet-a", "cidr_block" = "10.0.2.0/24", "availability_zone" = "${local.region_name}a", "map_public_ip_on_launch" = false }]
   nacl_assoc_list = ["public-subnet-a", "private-subnet-a"]
+}
+
+module "iam" {
+  source = "../modules/iam"
+
+  partition = local.partition_name
 }
