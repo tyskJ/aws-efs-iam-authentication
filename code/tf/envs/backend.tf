51ae32c0cebd3a084318094353824dd6f4953a05
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-03-13 21:14:13 +0900

    first settings

---
+++ code/tf/envs/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "tf-20250313"
+    key = "tfstate/terraform.tfstate"
+    profile = "admin"
+    region = "ap-northeast-1"
+  }
+}
---
+++ code/tf/envs/main.tf
@@ -0,0 +1,13 @@
+# ╔══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗
+# ║ EFS IAM Authentication Stack - Terraform main.tf module                                                                                          ║
+# ╠═════════════════╤═══════════════════════════════════╤════════════════════════════════════════════════════════════════════════════════════════════╣
+# ║ nw              │ ../modules/vpc_subnet             │ invoke vpc subnet module.                                                                  ║
+# ╚═════════════════╧═══════════════════════════════════╧════════════════════════════════════════════════════════════════════════════════════════════╝
+
+module "nw" {
+  source = "../modules/vpc_subnet"
+
+  vpc_map           = { "name" = "vpc", "cidr_block" = "10.0.0.0/16", "enable_dns_support" = true, "enable_dns_hostnames" = true }
+  subnet_map_list   = [{ "name" = "public-subnet-a", "cidr_block" = "10.0.1.0/24", "availability_zone" = "${local.region_name}a", "map_public_ip_on_launch" = true }, { "name" = "private-subnet-a", "cidr_block" = "10.0.2.0/24", "availability_zone" = "${local.region_name}a", "map_public_ip_on_launch" = false }]
+  nacl_assoc_list   = ["public-subnet-a", "private-subnet-a"]
+}
