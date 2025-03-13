43bfb56f9230f068e70386a4f2cc9fdde4682b80
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-03-13 23:41:28 +0900

    update output

--- code/tf/modules/iam/output.tf
+++ code/tf/modules/iam/output.tf
@@ -2,9 +2,15 @@
 # ║ EFS IAM Authentication Stack - Terraform output.tf output                                                                                        ║
 # ╠═════════════════════════════╤════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╣
 # ║ instanceprofile_name        │ Instance Profile Name.                                                                                             ║
+# ║ ec2_role_arn                │ EC2 IAM Role ARN.                                                                                                  ║
 # ╚═════════════════════════════╧════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝
 
 output "instanceprofile_name" {
   description = "EC2 Instance Profile Name"
   value       = aws_iam_instance_profile.ec2_instance_profile.name
+}
+
+output "ec2_role_arn" {
+  description = "EC2 IAM Role ARN"
+  value       = aws_iam_role.ec2_role.arn
 }
